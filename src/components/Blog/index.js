f30b0fe9d1108558d3f3e2beb6b0fef3bd9bbd49
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-06-23 10:33:06 +0500

    remove category

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -125,34 +125,6 @@ const Blog = () => {
           </Col>
           <Col xs="6">
             <FormGroup>
-              <Label for="category">category</Label>
-              <Input
-                type="select"
-                name="category"
-                id="category"
-                value={category}
-                onChange={(event) => {
-                  setCategory(event.target.value);
-                }}
-              >
-                <option>select</option>
-                <option value={"back-pain"}>Back Pain</option>
-                <option value={"leg-pain"}>Leg Pain</option>
-                <option value={"neck-pain"}>Neck Pain</option>
-                <option value={"knee-pain"}>Knee Pain</option>
-                <option value={"joint-pain"}>Joint Pain</option>
-                <option value={"shoulder-pain"}>Shoulder Pain</option>
-                <option value={"injury-treatment"}>Injury Treatment</option>
-                <option value={"health-tips"}>Health Tips</option>
-                <option value={"update"}>Update</option>
-                <option value={"treatments"}>Treatments</option>
-              </Input>
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs="6">
-            <FormGroup>
               <Label for="exampleEmail">Meta Description</Label>
               <Input
                 id="metaDes"
@@ -165,6 +137,8 @@ const Blog = () => {
               />
             </FormGroup>
           </Col>
+        </Row>
+        <Row>
           <Col xs="6">
             <FormGroup>
               <Label for="exampleFile">File</Label>
